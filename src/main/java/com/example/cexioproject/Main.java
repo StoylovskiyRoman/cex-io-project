03444a043e4f5682cada89252145def769c3c4e5
Roman Stoylovskiy <rstoylovskiy@ukr.net>
2022-05-08 16:17:30 +0200

    Use restTemplate to get response from cex.io

--- src/main/java/com/example/cexioproject/Main.java
+++ src/main/java/com/example/cexioproject/Main.java
@@ -1,7 +1,19 @@
 package com.example.cexioproject;
 
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpMethod;
+import org.springframework.web.client.RestTemplate;
+
 public class Main {
     public static void main(String[] args) {
+        RestTemplate restTemplate = new RestTemplate();
+        HttpHeaders headers2 = new HttpHeaders();
+        headers2.set("Accept", "application/json");
+        headers2.add("user-agent", "Mozilla/5.0 Firefox/26.0");
+
+        restTemplate.exchange("https://cex.io/api/currency_limits", HttpMethod.GET, new HttpEntity(headers2),
+                String.class).getBody();
 
     }
 }
